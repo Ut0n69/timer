20473045aeea4ffa784288d951836b988f851d30
yut0n <yt.sa.1117@gmail.com>
2017-06-26 02:28:19 +0900

    Fix: インスタンス生成を配列に変更 #28

--- www/js/main.js
+++ www/js/main.js
@@ -282,90 +282,17 @@ StopWatch.prototype.run = function() {
 
 $(function() {
 
-  var no1 = new StopWatch("No1");
-  var no2 = new StopWatch("No2");
-  var no3 = new StopWatch("No3");
-  var no4 = new StopWatch("No4");
-  var no5 = new StopWatch("No5");
-  var no6 = new StopWatch("No6");
-  var no7 = new StopWatch("No7");
-  var no8 = new StopWatch("No8");
-  var no10 = new StopWatch("No10");
-  var no11 = new StopWatch("No11");
-  var no12 = new StopWatch("No12");
-  var no13 = new StopWatch("No13");
-  var no14 = new StopWatch("No14");
-  var no15 = new StopWatch("No15");
-  var no16 = new StopWatch("No16");
-  var no17 = new StopWatch("No17");
-  var no18 = new StopWatch("No18");
-  var no19 = new StopWatch("No19");
-  var no20 = new StopWatch("No20");
-  var no21 = new StopWatch("No21");
-  var no22 = new StopWatch("No22");
-  var no23 = new StopWatch("No23");
-  var no24 = new StopWatch("No24");
-  var no25 = new StopWatch("No25");
-  var no26 = new StopWatch("No26");
-  var no27 = new StopWatch("No27");
-  var no28 = new StopWatch("No28");
-  var no29 = new StopWatch("No29");
-  var no30 = new StopWatch("No30");
-  var no31 = new StopWatch("No31");
-  var no32 = new StopWatch("No32");
-  var no33 = new StopWatch("No33");
-  var no34 = new StopWatch("No34");
-  var no35 = new StopWatch("No35");
-  var no36 = new StopWatch("No36");
-  var no37 = new StopWatch("No37");
-  var no38 = new StopWatch("No38");
-  var no39 = new StopWatch("No39");
-  var no40 = new StopWatch("No40");
-  var no41 = new StopWatch("No41");
-  var no42 = new StopWatch("No42");
-  var no43 = new StopWatch("No43");
-  var no44 = new StopWatch("No44");
-  var no45 = new StopWatch("No45");
-  var no46 = new StopWatch("No46");
-  var no47 = new StopWatch("No47");
-  var no48 = new StopWatch("No48");
-  var no49 = new StopWatch("No49");
-  var no50 = new StopWatch("No50");
-  var no51 = new StopWatch("No51");
-  var no52 = new StopWatch("No52");
-  var no53 = new StopWatch("No53");
-  var no54 = new StopWatch("No54");
-  var no55 = new StopWatch("No55");
-  var no56 = new StopWatch("No56");
-  var no57 = new StopWatch("No57");
-  var no58 = new StopWatch("No58");
-  var no59 = new StopWatch("No59");
-  var no60 = new StopWatch("No60");
-  var no61 = new StopWatch("No61");
-  var no62 = new StopWatch("No62");
-  var no63 = new StopWatch("No63");
-  var no64 = new StopWatch("No64");
-  var no65 = new StopWatch("No65");
-  var no66 = new StopWatch("No66");
-  var no67 = new StopWatch("No67");
-  var no68 = new StopWatch("No68");
-  var no69 = new StopWatch("No69");
-  var no70 = new StopWatch("No70");
-  var no71 = new StopWatch("No71");
-  var no72 = new StopWatch("No72");
-  var no73 = new StopWatch("No73");
-  var nectar = new StopWatch("Nectar");
-  var no76 = new StopWatch("No76");
-  var no77 = new StopWatch("No77");
-  var no78 = new StopWatch("No78");
-  var no79 = new StopWatch("No79");
-  var no80 = new StopWatch("No80");
-  var no81 = new StopWatch("No81");
-  var no82 = new StopWatch("No82");
-  var no83 = new StopWatch("No83");
-  var prometheus = new StopWatch("Prometheus");
-  var no86 = new StopWatch("No86");
+  // インスタンス生成
+  var arr = [];
+  var str;
 
+  var nectar = new StopWatch("Nectar");
+  var prometheus = new StopWatch("Prometheus");
+
+  for ( var i = 1; i <= 86; i++) {
+    str = String(i);
+    arr[i] = new StopWatch("No" + str);
+  }
 
 
   /*------------------
