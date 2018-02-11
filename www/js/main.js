4c30b6da720578c2baa1d4fa67ff58a8045e492d
yut0n <yt.sa.1117@gmail.com>
2018-02-12 02:01:04 +0900

    Update: デザインの変更

--- www/js/main.js
+++ www/js/main.js
@@ -10,40 +10,40 @@ var DEFAULT_TIME = 120;
 var socket = io.connect(location.origin);
 
 var URL;
-socket.on("webhook", function(data) {
+socket.on("webhook", function (data) {
   URL = data;
 });
 
 // スタイル変更関数
-var setEmpty = function(tableSelectar) {
+var setEmpty = function (tableSelectar) {
   $(tableSelectar).css({
     'background-color': "",
     'color': ''
   });
 };
 
-var setReserve = function(tableSelectar) {
+var setReserve = function (tableSelectar) {
   $(tableSelectar).css({
     'background-color': reserve,
     'color': '#fff'
   });
 };
 
-var setUse = function(tableSelectar) {
+var setUse = function (tableSelectar) {
   $(tableSelectar).css({
     'background-color': use,
     'color': '#000'
   });
 };
 
-var setWarn = function(tableSelectar) {
+var setWarn = function (tableSelectar) {
   $(tableSelectar).css({
     'background-color': warn,
     'color': '#000'
   });
 };
 
-var setOver = function(tableSelectar) {
+var setOver = function (tableSelectar) {
   $(tableSelectar).css({
     'background-color': over,
     'color': '#000'
@@ -51,37 +51,79 @@ var setOver = function(tableSelectar) {
 };
 
 // ハンバーガーメニュー
-var hbgMenu = function() {
+var hbgMenu = function () {
 
   $('body').prepend(
     '<div class="modal-bg">' +
     '<div class="hbgMenu">' +
     ' <div class="hbgMenu-close">' + '×' + '</div>' +
-    ' <div class="hbgMenu-reset">リセット</div>' +
-    ' <div class="hbgMenu-des">操作説明</div>' +
-    ' <div class="hbgMenu-err">エラー報告</div>' +
-    ' <div class="hbgMenu-log">操作ログ</div>' +
+    ' <div class="hbgMenu-reset">' +
+    ' <img width="80%" src="../images/reset.jpg">' +
+    '  <p style="margin: -20px;">リセット</p>' +
+    '</div>' +
+    ' <div class="hbgMenu-des"><img style="margin: 10px 10px 10px 0" width="84px" height="59px" src="../images/des.jpg"><p style="margin: 17px 0 0 0">操作説明</p></div>' +
+    ' <div class="hbgMenu-err"><img style="margin: 5px 3px 10px 3px" width="80px" height="68px" src="../images/err.jpg"><p style="margin: 17px 0 0 0">エラー報告</p></div>' +
+    ' <div class="hbgMenu-log"><img style="margin: 2px 6px 10px 6px" width="84px" height="76px" src="../images/log.jpg"><p style="margin: 17px 0 0 0">操作ログ</p></div>' +
     '</div>'
   );
 
   $(".modal-bg").fadeIn("fast");
   $(".hbgMenu").slideDown("fast");
 
-  $(".modal-bg, .hbgMenu-close").on("tap", function() {
+  $(".modal-bg, .hbgMenu-close").on("tap", function () {
     $(".modal-bg").remove();
+    $(".modal-contents").remove();
     $(".hbgMenu").remove();
 
     return false;
   });
 
-  $(".hbgMenu-reset").on("tap", function() {
-    socket.emit("event-reset");
-    location.reload();
+  $(".hbgMenu-reset").on("tap", function () {
+
+    $("body").prepend(
+      '<div class="modal-bg"></div>' +
+      '<div class="modal-contents">' +
+      '  <div style="font-size: 28px;" class="modal-contents-title">リセットしますか？</div>' +
+      '  <div class="modal-contents-selectar">' +
+      '    <div class="modal-contents-edit reset-no">いいえ</div>' +
+      '    <div class="modal-contents-end reset-yes" style="background: #ff9900;">はい</div>' +
+      '  </div>' +
+      '</div>'
+    );
+
+    $(".reset-yes").on("tap", function () {
+      $(".modal-contents").remove();
+      $(".modal-bg").remove();
+
+      $("body").prepend(
+        '<div class="modal-bg" style="background: #000; opacity: 0.8; display: inline"></div>' +
+        '<img style="z-index: 10; position: fixed; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%)" src="../images/nowloading.gif">'
+      );
+      socket.emit("event-reset");
+
+      setTimeout(function () {
+        location.reload();
+      }, 1000);
+
+      return false;
+    });
+
+    $(".reset-no").on("tap", function () {
+      $(".modal-bg").remove();
+      $(".modal-contents").remove();
+
+      return false;
+    });
+
+
+    $(".hbgMenu").remove();
+
+
 
     return false;
   });
 
-  $(".hbgMenu-des").on("tap", function() {
+  $(".hbgMenu-des").on("tap", function () {
     $('body').prepend(
       '<div class="howToUse">' +
       ' <p class="back">×</p>' +
@@ -92,7 +134,7 @@ var hbgMenu = function() {
     $(".modal-bg").remove();
     $(".hbgMenu").remove();
 
-    $(".back").on("tap", function() {
+    $(".back").on("tap", function () {
       $(".howToUse").remove();
 
       return false;
@@ -101,7 +143,7 @@ var hbgMenu = function() {
     return false;
   });
 
-  $(".hbgMenu-err").on("tap", function() {
+  $(".hbgMenu-err").on("tap", function () {
 
     $(".modal-bg").remove();
     $(".hbgMenu").remove();
@@ -120,7 +162,7 @@ var hbgMenu = function() {
       '</div>'
     );
 
-    $(".err-form-btn").on("tap", function() {
+    $(".err-form-btn").on("tap", function () {
       var errData = {};
       errData.date = moment().format('YYYYMMDD HH:mm');
       errData.name = $(".err-form-name").val();
@@ -165,7 +207,7 @@ var hbgMenu = function() {
         );
       }
 
-      $(".send-form-back").on("tap", function() {
+      $(".send-form-back").on("tap", function () {
         $(".howToUse").remove();
         return false;
       });
@@ -173,13 +215,13 @@ var hbgMenu = function() {
       return false;
     });
 
-    $(".back").on("tap", function() {
+    $(".back").on("tap", function () {
       $(".howToUse").remove();
       return false;
     });
   });
 
-  $(".hbgMenu-log").on("tap", function() {
+  $(".hbgMenu-log").on("tap", function () {
 
     // スクロール無効解除
     $(window).off('.noScroll');
@@ -189,26 +231,56 @@ var hbgMenu = function() {
     $(".modal-bg").remove();
     $(".hbgMenu").remove();
 
+
     $('body').prepend(
       '<div class="howToUse">' +
-      '<p class="back">×</p>' +
-      '<div class="log" style="overflow: scroll; background-color: #fff; widh: 100%; height: 80%; margin: 30px;">' +
+      '<h2 style="padding: 12px; background: #fff; height: 61px; margin: 0; z-index: 2; width: 100%; text-align: center; font-style: italic; position: fixed; top: 0; left: 50%; transform: translateX(-50%);">操作ログ</h2>' +
+      '<p style="z-index: 3; position: fixed; top: 0; left: 0;" class="back">×</p>' +
+      '<div class="log" style="overflow-scrolling: touch; -webkit-overflow-scrolling: touch; background-color: #fff; widh: 100%; height: 93%; margin: 62px 5px 5px 5px;">' +
+      '<table class="table-header">' +
+      '<tr>' +
+      '  <th style="width: 77px;">番号</th>' +
+      '  <th style="width: 77px;">操作</th>' +
+      '  <th style="width: 239px;">操作時間</th>' +
+      '  <th style="width: 113px;">残時間</th>' +
+      '  <th style="width: 113px;">編集前</th>' +
+      '  <th style="width: 113px;">編集後</th>' +
+      '</tr>' +
+      '</table>' +
+      '<div>' +
+      '<table class="table-log">' +
+      '</table>' +
+      '</div>' +
       '</div>' +
       '</div>'
     );
 
     socket.emit("getLog");
-    socket.on("catchLog", function(data) {
-        log = data;
-        for (i in log) {
-          $('.log').prepend('<p>' + log[i].log + '</p>');
-        }
+    socket.on("catchLog", function (data) {
+      log = data;
+      for (i in log) {
+        if (log[i].leftTime == null) log[i].leftTime = " - ";
+        if (log[i].beforeEdit == null) log[i].beforeEdit = " - ";
+        if (log[i].afterEdit == null) log[i].afterEdit = " - ";
 
+        $(".table-log").prepend(
+          '  <tr>' +
+          '   <td style="width: 77px;">' + log[i].num + '</td>' +
+          '   <td style="width: 81px;">' + log[i].operation + '</td>' +
+          '   <td style="width: 239px;">' + log[i].time + '</td>' +
+          '   <td style="width: 113px;">' + log[i].leftTime + '</td>' +
+          '   <td style="width: 113px;">' + log[i].beforeEdit + '</td>' +
+          '   <td style="width: 113px;">' + log[i].afterEdit + '</td>' +
+          '  </tr>'
+        )
+      }
     });
 
 
 
-    $(".err-form-btn").on("tap", function() {
+
+
+    $(".err-form-btn").on("tap", function () {
       var errData = {};
       errData.date = moment().format('YYYYMMDD HH:mm');
       errData.name = $(".err-form-name").val();
@@ -253,7 +325,7 @@ var hbgMenu = function() {
         );
       }
 
-      $(".send-form-back").on("tap", function() {
+      $(".send-form-back").on("tap", function () {
         $(".howToUse").remove();
         return false;
       });
@@ -261,11 +333,11 @@ var hbgMenu = function() {
       return false;
     });
 
-    $(".back").on("tap", function() {
+    $(".back").on("tap", function () {
       $(".howToUse").remove();
 
       // スクロール無効
-      $(window).on('touchmove.noScroll', function(e) {
+      $(window).on('touchmove.noScroll', function (e) {
         e.preventDefault();
       });
 
@@ -277,12 +349,13 @@ var hbgMenu = function() {
 
 
 // モーダル
-var modal = function(num) {
+var modal = function (num) {
 
   $("body").prepend(
     '<div class="modal-bg"></div>' +
     '<div class="modal-contents">' +
     '  <div class="modal-contents-title"></div>' +
+    '  <div class="modal-contents-close" style="color: #fff; font-size: 50px; position: fixed; top: -12px; right: 9px";>×</div>' +
     '  <div class="modal-contents-selectar">' +
     '    <div class="modal-contents-edit">編集</div>' +
     '    <div class="modal-contents-end">終了</div>' +
@@ -300,7 +373,7 @@ var modal = function(num) {
   $bg.fadeIn("fast");
 
   // カウントダウン終了
-  $(".modal-contents-end").on("tap", function() {
+  $(".modal-contents-end").on("tap", function () {
     socket.emit("event-end", num);
 
     $contents.remove();
@@ -310,30 +383,51 @@ var modal = function(num) {
   });
 
   // 編集
-  $(".modal-contents-edit").on("tap", function() {
+  $(".modal-contents-edit").on("tap", function () {
+    var currentNum = $(".no" + num)[0].innerText;
+
+    // スクロール無効解除
+    $(window).off('.noScroll');
 
     $contents.remove();
 
     $("body").prepend(
       '<div class="modal-edit">' +
       '  <div class="modal-contents-title">' + num + '</div>' +
+      '  <div class="modal-contents-close" style="color: #fff; font-size: 50px; position: fixed; top: -12px; right: 9px";>×</div>' +
       '  <div class="modal-contents-selectar">' +
-      '     <select class="modal-edit-selectar">' +
-      '        <option class="modal-edit-val">120</option>' +
-      '      </select>' +
+      '    <div class="input-val">' + Number(currentNum) + '</div>' +
+      '    <input class="input-range" value="' + Number(currentNum) + '" min="1" max="120" type="range">' +
       '  </div>' +
-      '   <div class="modal-edit-conf">確定</div>' +
+      '  <div class="modal-edit-conf">確定</div>' +
       '</div>'
     );
 
-    for (i = 119; i > 0; i--) {
-      $(".modal-edit-selectar").append(
-        '<option class="modal-edit-val">' + i + '</option>'
-      );
-    }
+    $(".modal-contents-close, .modal-bg").on("tap", function () {
 
-    $(".modal-edit-conf").on("tap", function() {
-      var time = $(".modal-edit-selectar option:selected").text();
+      // スクロール無効
+      $(window).on('touchmove.noScroll', function (e) {
+        e.preventDefault();
+      });
+
+      $(".modal-edit").remove();
+      $contents.remove();
+      $bg.remove();
+
+      return false;
+    });
+
+    $(".input-range").on("input change", function () {
+      $(".input-val").text($(".input-range").val());
+    });
+
+    $(".modal-edit-conf").on("tap", function () {
+      // スクロール無効
+      $(window).on('touchmove.noScroll', function (e) {
+        e.preventDefault();
+      });
+
+      var time = $(".input-range").val();
       var info = {
         "num": num,
         "time": time
@@ -344,32 +438,30 @@ var modal = function(num) {
       $(".modal-edit").remove();
       $bg.remove();
 
-
-      return false;
-    });
-
-    // 背景タップで閉じる
-    $bg.on("tap", function() {
-      $(".modal-edit").remove();
-      $bg.remove();
-
       return false;
     });
 
     return false;
   });
 
+  // 背景タップ or ☓ で閉じる
+  $(".modal-contents-close, .modal-bg").on("tap", function () {
 
-  // 背景タップで閉じる
-  $bg.on("tap", function() {
+    // スクロール無効
+    $(window).on('touchmove.noScroll', function (e) {
+      e.preventDefault();
+    });
+
+    $(".modal-edit").remove();
     $contents.remove();
     $bg.remove();
 
     return false;
   });
+
 };
 
-$(function() {
+$(function () {
 
   // 座席の生成
   for (i = 1; i <= 86; i++) {
@@ -387,7 +479,7 @@ $(function() {
 
   // サーバーから座席情報を取得
   socket.emit("before");
-  socket.on("catchStatus", function(data) {
+  socket.on("catchStatus", function (data) {
     for (i in data) {
 
       // 空席
@@ -424,12 +516,12 @@ $(function() {
   });
 
   // 1ミリ秒毎に更新
-  setInterval(function() {
+  setInterval(function () {
     socket.emit("before");
   }, DEFAULT_INTERVAL);
 
   // テーブルイベント
-  $(".table > *").on("tap", function(e) {
+  $(".table > *").on("tap", function (e) {
 
     var num = e.currentTarget.className;
     var bg = $("." + num).css('background-color');
@@ -452,10 +544,10 @@ $(function() {
           bgColor: '#444444',
           textColor: '#eeeeee',
           textAlign: 'left',
-          beforeShow: function() {},
-          afterShown: function() {},
-          beforeHide: function() {},
-          afterHidden: function() {}
+          beforeShow: function () {},
+          afterShown: function () {},
+          beforeHide: function () {},
+          afterHidden: function () {}
         });
       } else {
         modal(num.substr(2));
@@ -470,12 +562,12 @@ $(function() {
   $('div').css('user-select', 'none').on('copy paste contextmenu', false);
 
   // スクロール無効
-  $(window).on('touchmove.noScroll', function(e) {
+  $(window).on('touchmove.noScroll', function (e) {
     e.preventDefault();
   });
 
   // ハンバーガーメニュー
-  $(".header-menu").on("tap", function() {
+  $(".header-menu").on("tap", function () {
     hbgMenu();
 
     return false;
